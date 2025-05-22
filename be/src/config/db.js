90a128923490ddfa6e683b3e682e3c9da712c764
Tuan Anh 04 <tuanen047@gmail.com>
2025-05-22 17:28:39 +0700

    fix: deloy

--- be/src/config/db.js
+++ be/src/config/db.js
@@ -1,12 +1,12 @@
-// config/db.js
 const mongoose = require("mongoose");
-
 const connectDB = async (uri) => {
-  return mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 20000, // Hoặc cao hơn nếu cần
-  });
+  try {
+    await mongoose.connect(uri);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = { connectDB };
+module.exports = {
+  connectDB,
+};
