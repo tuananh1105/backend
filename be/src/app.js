3ffb3eaa0c1a210dddee6fb4429cf6b58b63cc64
Tuan Anh 04 <tuanen047@gmail.com>
2025-05-23 11:29:55 +0700

    fix: deloy

--- be/src/app.js
+++ be/src/app.js
@@ -28,10 +28,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://project-nextjs-qos7.vercel.app"
-    ],
+    origin: "https://project-nextjs-qos7.vercel.app", 
     credentials: true,
   },
 }); 
