6e392a2ac4bc24be538c364dfb2b94dd4fd7a87c
Tuan Anh 04 <tuanen047@gmail.com>
2025-05-23 11:04:35 +0700

    fix: deloy

--- be/src/app.js
+++ be/src/app.js
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // hoặc domain frontend thực tế khi deploy
+    origin: "https://project-nextjs-qos7.vercel.app/", 
     credentials: true,
   },
 });
